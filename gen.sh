dcba167a90f17ab438f7e5c6ad49184f61920a88
Russell Bryan <bryanr@asmsa.org>
2014-04-10 16:47:44 -0500

    added generator

---
+++ gen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for i in $(seq -f "%03g"  0 100); do
+	for j in $(seq -f "%03g"  0 100); do
+		for k in $(seq -f "%03g"  0 100); do
+			echo "./crot anin.png $i-$j-$k.png 0.$i 0.$j 0.$k;";
+			./crot anin.png $i-$j-$k.png 0.$i 0.$j 0.$k;
+		done
+	done
+done
