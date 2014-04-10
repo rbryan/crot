2d0aedd0a1f6a0aabb80a3ace8f5511a0f9e2bcc
Russell Bryan <bryanr@asmsa.org>
2014-04-10 16:51:39 -0500

    fixed gen slash thing

--- gen.sh
+++ gen.sh
@@ -3,7 +3,7 @@
 for i in $(seq -f "%03g"  0 100); do
 	for j in $(seq -f "%03g"  0 100); do
 		for k in $(seq -f "%03g"  0 100); do
-			echo "./crot $1 $2/$i-$j-$k.png 0.$i 0.$j 0.$k;";
+			echo "./crot $1 $2$i-$j-$k.png 0.$i 0.$j 0.$k;";
 			./crot anin.png $i-$j-$k.png 0.$i 0.$j 0.$k;
 		done
 	done
