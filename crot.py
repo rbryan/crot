ce35b2369d597741c0055932258bd7ddd8e3b7e7
Russell Bryan <bryanr@asmsa.org>
2014-04-10 00:11:23 -0500

    done

---
+++ crot.py
@@ -0,0 +1,68 @@
+import Image
+import sys
+import math
+
+
+def main():
+	if len(sys.argv) < 2:
+		print "Please name a file to rotate."
+		sys.exit(0);
+
+	img = Image.open(sys.argv[1]);
+	pix = img.load();
+	w,h = img.size;
+
+	for i in xrange(w):
+		for j in xrange(h):
+			p = pix[i,j];
+			pix[i,j] = rot(p,1*dist(0,0,i,j)/1000,1*dist(0,0,i,j)/1000,1*dist(0,0,i,j)/1000);
+	img.save("output.jpg");
+
+def dist(a,b,c,d):
+	return math.sqrt((a-c)**2+(b-d)**2);
+
+def rotx(p,x):
+	r1,g1,b1=p;
+	r = r1;
+	g = math.cos(x)*g1 - math.sin(x)*b1;
+	b = math.sin(x)*g1 + math.cos(x) *b1;
+	r = abs(r);
+	g = abs(g);
+	b = abs(b);
+	p = r,g,b;
+	return p;
+
+def rotz(p,z):
+	r1,g1,b1=p;
+	r = r1*math.cos(z) + math.sin(z) *g1;
+	g = -math.sin(z)*r1 + math.cos(z) *g1;
+	b = b1;
+	r = abs(r);
+	g = abs(g);
+	b = abs(b);
+	p = r,g,b;
+	return p;
+
+def roty(p,y):
+	r1,g1,b1=p;
+	r = r1*math.cos(y) + math.sin(y) * b1;
+	g = g1;
+	b = -math.sin(y)*r1 + math.cos(y) *b1;
+	r = abs(r);
+	g = abs(g);
+	b = abs(b);
+	p = r,g,b;
+	return p;
+
+def rot(p,x,y,z):
+	p = rotx(p,x);
+	p = roty(p,y);
+	p = rotz(p,z);
+	r,g,b = p;
+	r = int(r)%255;
+	g = int(g)%255;
+	b = int(b)%255;
+	p = r,g,b;
+	return p;
+
+main();	
